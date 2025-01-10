6ea802dafd4c149e2f82ae42ce87ea4fc7890c0c
Cairon29 <josewjjunco@gmail.com>
2025-01-10 01:31:24 -0500

    feat: Documentation and basic exercices added to typescript-practice

--- typescript-practice/index.ts
+++ typescript-practice/index.ts
@@ -1,5 +1,17 @@
 /* TypeScript basics */
 
+/* 
+  NOTES:
+  - We cannot run TypeScript files directly in the browser. We need to compile them to JavaScript first.
+  - We can use the TypeScript compiler to compile the TypeScript files to JavaScript files.
+  - We can use the tsc command to compile the TypeScript files.
+  - The TypeScript compiler will generate a .js file for each .ts file.
+  - We can also use the watch mode to automatically compile the TypeScript files when they change.
+  - We can use the tsc -w command to run the TypeScript compiler in watch mode.
+*/
+
+
+/*   BASIC TYPING   */
 // We can use the implicit type declaration as in javascript
 let number = 5;
 let string = "Hello";
@@ -9,8 +21,67 @@ let numberVar: number;
 let stringVar: string;
 let booleanVar: boolean; 
 let anyVar: any; // This is not recommended as it can lead to runtime errors since it can receive any kind of value
+let unknownVar: unknown; // This is a better alternative to any since it forces us to check its type before using it
 
-const addNumbers = (x: number, y: number) => {
-  return x + y;
+/*   TYPE ASSERTION   */
+let randomValue : unknown = 10;
+let value1 = randomValue as string;
+let value2 = (randomValue as string).toUpperCase();
+// This is like a value convertion. We are telling the compiler that we know the type of the value and we want to use it as an specific type.
+
+// The second way to translate a value from one type to another is using the <angle-bracket> syntax
+let value3 = <string>randomValue;
+
+/*  UNION TYPES   */
+// We can use the | operator to declare a variable that can hold a value of one of the types
+let unionValue: string | number;
+unionValue = "Hello"; // valid
+unionValue = 10;      // valid
+// unionValue = false;   // invalid
+
+    // Example
+const add = (x: number | string, y: number | string) => {
+  if (typeof x === 'number' && typeof y === 'number') {
+      return x + y;
+  }
+  if (typeof x === 'string' && typeof y === 'string') {
+      return x.concat(y);
+  }
+  throw new Error('Parameters must be numbers or strings');
 }
-console.log(addNumbers(3, 6));
+// console.log(add('one', 'two'));  //* Returns "onetwo"
+// console.log(add(1, 2));          //* Returns 3
+// console.log(add('one', 2));      //* Returns error
+
+
+/*   INTERFACES   */
+
+// Interfaces allow us to define the structure of an object and use it for type its properties
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Bachelor {
+  degree: string;
+}
+
+const Nataly : Person = {
+  age: 30,
+  name: 'Nataly'
+}
+
+// Here we are using two different interfaces to define the structure of the object
+const Johan : Person & Bachelor = {
+  name: 'Johan',
+  age: 30,
+  degree: 'Professional chef'
+}
+
+/*   Exercise - enums   */
+// Create a set of values that you can use in your code. 
+enum ContractStatus {
+  Permanent,
+  Temp,
+  Apprentice
+}
