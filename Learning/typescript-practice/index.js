2fbe4fc736afea71ae847681b00c27bc176d6fa8
Cairon29 <josewjjunco@gmail.com>
2025-01-13 22:23:16 -0500

    feat: refactoring-code/mslearn-typescript module 2 completed

--- Learning/typescript-practice/index.js
+++ Learning/typescript-practice/index.js
@@ -70,3 +70,5 @@ var ContractStatus;
     ContractStatus[ContractStatus["Temp"] = 1] = "Temp";
     ContractStatus[ContractStatus["Apprentice"] = 2] = "Apprentice";
 })(ContractStatus || (ContractStatus = {}));
+
+
