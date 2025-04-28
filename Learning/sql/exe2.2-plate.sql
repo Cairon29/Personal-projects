cc999d5a68674ae3f8d00a8f2121216635fe2359
Cairon29 <josewjjunco@gmail.com>
2025-04-27 22:53:37 -0500

    feat: first inner join added

--- Learning/sql/exe2.2-plate.sql
+++ Learning/sql/exe2.2-plate.sql
@@ -34,3 +34,19 @@ ADD user_id int;
 ALTER TABLE bikes
 ADD CONSTRAINT fk_user
 FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+INSERT INTO bikes (brand, max_speed) values ('kawasaki', 340), ('ducati', 350), ('bmw', 300);
+INSERT INTO users (name) values ('johan'), ('ajelandro'), ('Arley');
+INSERT INTO plates (plate_number, city) VALUES ('ACS32', 'Bogota DC'), ('XF23D3', 'Cali'), ('RTV876', 'Medellin');
+
+UPDATE bikes SET user_id = 3 where id = 2;
+UPDATE bikes SET plate_id = 1 where id = 1;
+UPDATE bikes SET plate_id = 2 where id = 2;
+UPDATE bikes SET plate_id = 3 where id = 3;
+
+SELECT * from bikes;
+
+SELECT * from bikes
+JOIN users
+on user_id = users.id ORDER BY max_speed DESC;
