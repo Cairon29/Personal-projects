d97765313e2575ad4f4c026547cd6f18d401a41d
Cairon29 <josewjjunco@gmail.com>
2025-04-28 23:51:26 -0500

    feat: INNER JOIN, LEFT JOIN, RIGHT JOIN, added

--- Learning/sql/exe2.2-plate.sql
+++ Learning/sql/exe2.2-plate.sql
@@ -50,3 +50,38 @@ SELECT * from bikes;
 SELECT * from bikes
 JOIN users
 on user_id = users.id ORDER BY max_speed DESC;
+
+-- It does not matter the order in which we place the 'on' value closures
+
+SELECT * from bikes
+JOIN users
+on users.id = bikes.user_id ORDER BY max_speed DESC;
+
+
+-- We can skip the 'inner' closure since in most of DB engines support only using JOIN in this statements
+
+SELECT * from bikes
+INNER JOIN users
+on user_id = users.id ORDER BY max_speed DESC;
+
+-- LEFT JOIN
+
+-- It makes the SQL consult keep not only the common values but also the 'from' table values (in this case bikes table)
+SELECT * from bikes
+LEFT JOIN users
+on user_id = users.id ORDER BY max_speed DESC;
+
+
+-- In this case it works the same as the LEFT JOIN but keeps the not common values of the evaluated table (in this case users table)
+SELECT * from bikes
+RIGHT JOIN users
+on user_id = users.id ORDER BY max_speed DESC;
+
+
+-- In this case we are accessing the data as if it was an object
+SELECT (bikes.brand, users.id, users.name) AS custom_consult from bikes
+JOIN users
+on user_id = users.id ORDER BY max_speed DESC;
+
+-- imagine both columns are not null, when we insert it will be necessary to provide a value for each argument
+INSERT INTO bikes (value1, value2) VALUES ('here value 1', 'here value 2')
