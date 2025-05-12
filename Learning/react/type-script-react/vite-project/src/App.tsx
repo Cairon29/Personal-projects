f3eb2dbdb7a9c18303abf91b6edd3ec668f13124
Cairon29 <josewjjunco@gmail.com>
2025-05-11 23:19:22 -0500

    feat: react + typescript practice added

---
+++ Learning/react/type-script-react/vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const App: React.FC = () => {
+
+  return (
+    <>
+      the thing is that newer versions of vite are not generating the eslintrc file
+    </>
+  )
+}
+
+export default App
