86d0f8d80b7dd5230c08e9e98e02b1ee270ba6b5
Cairon29 <josewjjunco@gmail.com>
2025-10-15 21:45:02 -0500

    feat: ML exercises added implementing also MLFlow library

--- Learning/sklearn/index.py
+++ Learning/sklearn/index.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -5,10 +6,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.metrics import r2_score
-
-import numpy as np
 
 
 california = fetch_california_housing()
@@ -20,7 +19,10 @@ california_df['MedHouseVal'] = california.target
 print(california_df)
 california_df.head()
 
-data = california_df.drop('MedHouseVal', axis=1)
+# MedInc  HouseAge
+
+# data = california_df.drop('MedHouseVal', axis=1)
+data = california_df[['MedInc', 'HouseAge']]
 values = california_df[['MedHouseVal']]
 
 x = np.array(data)
@@ -34,20 +36,31 @@ sc = StandardScaler()
 x_train_std = sc.fit_transform(x_train)
 x_test_std = sc.transform(x_test)
 
+mlflow.set_tracking_uri("./mlruns")
+mlflow.set_experiment("california_housing_experiment")
+
 with mlflow.start_run():
     
     model = LinearRegression()
     model.fit(x_train_std, y_train)
     y_pred = model.predict(x_test_std)
     
-    mse = mean_squared_error(y_test, y_test)
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    
     n_estimators = 100
       
     mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_metric("rmse", mse)
+    mlflow.log_metric("mae", mae)
+    mlflow.log_metric("R2 score", r2)
+    mlflow.log_metric("prueba", 21212)
     mlflow.sklearn.log_model(model, "model")
     print(y_pred)
-    # print("MSE:", mean_squared_error(y_test, y_test))
-    # print('MAE', mean_absolute_error(y_test, y_pred))
+    print("MSE:",mse)
+    print('MAE', mae)
     print('F2 score', r2_score(y_test, y_pred))
 
+    
+print('testing finished run')
